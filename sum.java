2da19c3200fc02a415a85f6c986d4f131fd98c76
Santhosh0077 <141298674+Santhosh0077@users.noreply.github.com>
2023-12-12 08:52:30 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; 
+public class sum{ public static void main(String args[]) {
+     	Scanner reader = new Scanner(System.in); 
+     	 
+     	 
+
+     	
+     		System.out.println("Enter value for a: ");
+     		
+     		
+     		 int a = reader.nextInt();  
+     		 
+     		 
+     		  System.out.println("enter value for b: ");
+     		  
+     		   int b = reader.nextInt(); 
+     		    
+     		    double sum = a+b;
+     		   
+     		   System.out.println("sum"); } } 
